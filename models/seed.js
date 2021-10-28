b50c720271fecad61c9dc8586c38146c1332fca4
wensydesousa <wensys013@gmail.com>
2021-10-28 07:29:19 -0700

    .

--- models/seed.js
+++ models/seed.js
@@ -18,9 +18,9 @@ const db = mongoose.connection
 db.on("open", () => {
     // array of starter visions
     const startVisions = [
-        { title: "this is Malibu", image: "https://images.unsplash.com/photo-1499898595565-f424ed1d075c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80", complete: false },
-        { title: "this is Malibu", image: "https://images.unsplash.com/photo-1499898595565-f424ed1d075c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80", complete: false },
-        { title: "this is Malibu", image: "https://images.unsplash.com/photo-1499898595565-f424ed1d075c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80", complete: false },
+        { title: "this is Malibu", image: "https://images.unsplash.com/photo-1499898595565-f424ed1d075c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80", description: "this is a future trip I want to take!", complete: false },
+        { title: "this is Malibu", image: "https://images.unsplash.com/photo-1499898595565-f424ed1d075c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80", description: "this is a future trip I want to take!", complete: false },
+        { title: "this is Malibu", image: "https://images.unsplash.com/photo-1499898595565-f424ed1d075c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",description: "this is a future trip I want to take!", complete: false },
       ];
 
     // delete all visions

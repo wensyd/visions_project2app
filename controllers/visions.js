86b260bdb8bc619e1349a08894133d5039ef4d40
wensydesousa <wensys013@gmail.com>
2021-10-28 10:40:41 -0700

    index route added

--- controllers/visions.js
+++ controllers/visions.js
@@ -3,7 +3,7 @@
 ////////////////////////
 
 const express = require("express")// express for router function 
-const Visions = require("../models/Visions.js")// fruit model
+const Visions = require("../models/Visions.js")// vision model
 
 
 ////////////////////////////
@@ -16,15 +16,15 @@ const router = express.Router()
 // Router Middleware
 ////////////////////////////
 
-router.use((req, res, next)=> {
-    // check if logged in
-    if (req.session.loggedIn){
-        // send to routes
-        next()
-    } else {
-        res.redirect("/user/login")
-    }
-});
+// router.use((req, res, next)=> {
+//     // check if logged in
+//     if (req.session.loggedIn){
+//         // send to routes
+//         next()
+//     } else {
+//         res.redirect("/user/login")
+//     }
+// });
 
 
 
@@ -35,12 +35,13 @@ router.use((req, res, next)=> {
 //index route  get /fruits
 
 router.get("/", (req, res) => {
-    // find all the fruits
-    Fruit.find({username: req.session.username})
-    .then((fruits) => {
-        // render the index template with the fruits
-        res.render("fruits/index.liquid", {fruits})
+    // find all the visions
+    Visions.find({})
+    .then((visions) => {
+        // render the index template with the visions
+        res.render("visions/index.liquid", {visions})
     })
+
       //error handling 
 
       .catch((error) => {
@@ -50,103 +51,103 @@ router.get("/", (req, res) => {
     });
 
     // new route
-router.get("/new", (req, res) => {
-    res.render("fruits/new.liquid");
-  });
+// router.get("/new", (req, res) => {
+//     res.render("fruits/new.liquid");
+//   });
 
   // create - post request - /fruits
-router.post("/", (req, res) => {
+// router.post("/", (req, res) => {
 
-    // convert the checkbox property to true or false
-    req.body.readyToEat = req.body.readyToEat === "on" ? true : false
+//     // convert the checkbox property to true or false
+//     req.body.readyToEat = req.body.readyToEat === "on" ? true : false
 
-     // add the username to req.body, to track user
-     req.body.username = req.session.username
+//      // add the username to req.body, to track user
+//      req.body.username = req.session.username
      
-    // create the new fruit
-    Fruit.create(req.body)
-    .then((fruit) => {
-        // redirect the user back to the index route
-        res.redirect("/fruits")
-    })
-    // error handling
-    .catch((error) => {
-        res.json({error})
-    })
+//     // create the new fruit
+//     Fruit.create(req.body)
+//     .then((fruit) => {
+//         // redirect the user back to the index route
+//         res.redirect("/fruits")
+//     })
+//     // error handling
+//     .catch((error) => {
+//         res.json({error})
+//     })
 
-})
+// })
 
 // edit route - get request - /fruits/:id/edit
-router.get("/:id/edit", (req, res) => {
-    // get the id from params
-    const id = req.params.id
+// router.get("/:id/edit", (req, res) => {
+//     // get the id from params
+//     const id = req.params.id
 
-    // get the fruit with the matching id
-    Fruit.findById(id)
-    .then((fruit) => {
-        // render the edit page template with the fruit data
-        res.render("fruits/edit.liquid", { fruit })
-    })
-    // error handling
-    .catch((error) => {
-        res.json({error})
-    })
-})
+//     // get the fruit with the matching id
+//     Fruit.findById(id)
+//     .then((fruit) => {
+//         // render the edit page template with the fruit data
+//         res.render("fruits/edit.liquid", { fruit })
+//     })
+//     // error handling
+//     .catch((error) => {
+//         res.json({error})
+//     })
+// })
 
 // update route - put request - "/fruits/:id"
-router.put("/:id", (req, res) => {
+// router.put("/:id", (req, res) => {
     // get the id from params
-    const id = req.params.id
+    // const id = req.params.id
     
     // convert the checkbox property to true or false
-    req.body.readyToEat = req.body.readyToEat === "on" ? true : false
+    // req.body.readyToEat = req.body.readyToEat === "on" ? true : false
 
     // update the item with the matching id
-    Fruit.findByIdAndUpdate(id, req.body, {new: true})
-    .then((fruit) => {
-        // redirect user back to index
-        res.redirect("/fruits")
-    })
-     // error handling
-     .catch((error) => {
-        res.json({error})
-    })
-})
+//     Fruit.findByIdAndUpdate(id, req.body, {new: true})
+//     .then((fruit) => {
+//         // redirect user back to index
+//         res.redirect("/fruits")
+//     })
+//      // error handling
+//      .catch((error) => {
+//         res.json({error})
+//     })
+// })
 
 //destroy route - delete request -/ fruits/:id
 
-router.delete("/:id", (req, res)=> {
-    // grab the id from params
-    const id = req.params.id
-    // delete the fruit
-    Fruit.findByIdAndRemove(id)
-    .then((fruit) => {
-        // redirect user back to index
-        res.redirect("/fruits")
-    })
-     // error handling
-     .catch((error) => {
-        res.json({error})
-    })
-})
+// router.delete("/:id", (req, res)=> {
+//     // grab the id from params
+//     const id = req.params.id
+//     // delete the fruit
+//     Fruit.findByIdAndRemove(id)
+//     .then((fruit) => {
+//         // redirect user back to index
+//         res.redirect("/fruits")
+//     })
+//      // error handling
+//      .catch((error) => {
+//         res.json({error})
+//     })
+// })
     
     // show route - get - /fruits/:id
 
-    router.get("/:id", (req, res)=> {
-        //get the id from params
-        const id = req.params.id
+    // router.get("/:id", (req, res)=> {
+    //     //get the id from params
+    //     const id = req.params.id
 
-        // get that particular fruit from the database
-        Fruit.findById(id)
-        .then((fruit) => {
-            // render the show template with the fruit
-            res.render("fruits/show.liquid", {fruit})
-        })
-        // error handling
-        .catch((error) => {
-            res.json({error})
-        })
-    })
+    //     // get that particular fruit from the database
+    //     Fruit.findById(id)
+    //     .then((fruit) => {
+    //         // render the show template with the fruit
+    //         res.render("fruits/show.liquid", {fruit})
+    //     })
+    //     // error handling
+    //     .catch((error) => {
+    //         res.json({error})
+    //     })
+    // })
  
 
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express") // web framework
 const morgan = require("morgan") // logger
 const methodOverride = require("method-override") // to swap request methods
 const path = require("path") // helper functions for file paths
-const FruitsRouter = require("./controllers/fruit")
+const visionsRouter = require("./controllers/visions")
 const UserRouter = require("./controllers/user")
 const session = require("express-session") // session middleware
 const MongoStore = require("connect-mongo") // save sessions in mongo
@@ -39,7 +39,8 @@ app.use(morgan("tiny")); //logging
 app.use(methodOverride("_method")); // override for put and delete requests from forms
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
-// middlware to create sessions (req.session)
+
+// middleware to create sessions (req.session)
 app.use(session({
     secret: process.env.SECRET,
     store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
@@ -51,7 +52,7 @@ app.use(session({
 ///////////
 
 app.get("/", (req, res) => {
-    res.render("index.liquid")
+    res.send("Hello")
 })
 
 // register visions router
@@ -59,7 +60,7 @@ app.use("/visions", visionsRouter)
 
 // register user router
 
-app.use("/user", UserRouter)
+// app.use("/user", UserRouter)
 
 
 //////////////
